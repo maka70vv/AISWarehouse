234d344e032167b2a17c1aaf14b8f9fa1c626351
maka70vv <25.makarovv@gmail.com>
2024-12-21 12:56:41 +0600

    maximized window & database config from app

---
+++ build/CMakeFiles/AISWarehouseCPP.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "AISWarehouseCPP_autogen"
+  "CMakeFiles/AISWarehouseCPP_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/AISWarehouseCPP_autogen.dir/ParseCache.txt"
+  "AISWarehouseCPP"
+  "AISWarehouseCPP.pdb"
+  "CMakeFiles/AISWarehouseCPP.dir/AISWarehouseCPP_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/AISWarehouseCPP.dir/AISWarehouseCPP_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/AISWarehouseCPP.dir/DatabaseManager.cpp.o"
+  "CMakeFiles/AISWarehouseCPP.dir/DatabaseManager.cpp.o.d"
+  "CMakeFiles/AISWarehouseCPP.dir/MainWindow.cpp.o"
+  "CMakeFiles/AISWarehouseCPP.dir/MainWindow.cpp.o.d"
+  "CMakeFiles/AISWarehouseCPP.dir/main.cpp.o"
+  "CMakeFiles/AISWarehouseCPP.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AISWarehouseCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
